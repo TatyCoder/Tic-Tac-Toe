7e2ac424c0052f3a0fa7bc855cbf19749c7ce850
Tatiana Domingue <tatiana.domingue@protonmail.com>
2021-12-24 12:12:32 -0600

    How to handle player choice

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Tic-Tac-Toe
 This is the general approach I want to follow:
 
-Basically, the Tic Tac Toe can be represented by 3 arrays that represent the rows, each one with 3 columns.
+Basically, the Tic Tac Toe can be represented by three arrays that represent the rows, each one with three columns.
 Each time when one of the players click on the board I want to modify the arrays to capture their choice. At this time I want to validate their choices and print the messages to match the rules of the game. When this is completed I want to update the user interface.
 
 I'm going to create the html file to display the basic design of the Tic Tac Toe game.
+
+
+## How to handle player choice:
+This game is going to have two buttons named "Player X" and "Player O". The first player that chooses a button initiates the game, and buttons get disable at this point.
--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const row1 = ["O", "X", "Blank"];
+const row1 = ["", "", ""];
 
-const row2 = ["X", "Blank", "O"];
+const row2 = ["", "", ""];
 
-const row3 = ["Blank", "o", "x"];
+const row3 = ["", "", ""];
 
 const render = () => {
     for (let i = 0; i < row1.length; i++) {
@@ -10,18 +10,60 @@ const render = () => {
         const cell = document.querySelector("#r1c" + (i+1));
         cell.setAttribute("src", element + ".png");
     }
-
+    
     for (let i = 0; i < row2.length; i++) {
         const element = row2[i];
         const cell = document.querySelector("#r2c" + (i+1));
         cell.setAttribute("src", element + ".png");
     }
-
+    
     for (let i = 0; i < row3.length; i++) {
         const element = row3[i];
         const cell = document.querySelector("#r3c" + (i+1));
         cell.setAttribute("src", element + ".png");
     }
+}
+
+const clearBoard = () => {
+    for (let i = 0; i < row1.length; i++) {
+        row1[i] = "Blank";
+    }
+    for (let i = 0; i < row2.length; i++) {
+        row2[i] = "Blank";
+    }
+    for (let i = 0; i < row3.length; i++) {
+        row3[i] = "Blank";
+    }
+}
+
+clearBoard();
+
+let currentPlayer = "";
+
+const chooseX = document.querySelector("#buttonX");
+
+chooseX.addEventListener("click", () => {
+    currentPlayer = "X";
+    document.querySelector("#playerSelection").setAttribute("style", "display: none");
+    document.querySelector("#board").setAttribute("style", "display: block");
+});
+
+const chooseO = document.querySelector("#buttonO");
+
+chooseO.addEventListener("click", () => {
+    currentPlayer = "O";
+    document.querySelector("#playerSelection").setAttribute("style", "display: none");
+    document.querySelector("#board").setAttribute("style", "display: block");
+});
+
+const clickCell = (row, column) => {
+    row[column] = currentPlayer;
+    if (currentPlayer === "O") {
+        currentPlayer = "X";
+    } else {
+        currentPlayer = "O";
+    }
+    render();
 }
 
 render();
