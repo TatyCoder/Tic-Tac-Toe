e4dd6445c818b357ade9dfd3c3cf13d2dcfe47a9
Tatiana Domingue <tatiana.domingue@protonmail.com>
2021-12-25 16:21:38 -0600

    Set the validation of the player's choice

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Tic-Tac-Toe
+# Tic Tac Toe
 This is the general approach I want to follow:
 
 Basically, the Tic Tac Toe can be represented by three arrays that represent the rows, each one with three columns.
-Each time when one of the players click on the board I want to modify the arrays to capture their choice. At this time I want to validate their choices and print the messages to match the rules of the game. When this is completed I want to update the user interface.
+Each time when one of the players click on the board I want to modify the arrays to capture their choice. At this time I want to validate their choices and print the messages to match the rules of the game. When this is completed I want to update the user interface by using render().
+
+The design pattern works like this: everytime the player interacts with the user interface there is an event listener that modifies the application state or data, and then the render function refresh the html. Only the render function can update the html. 
 
 I'm going to create the html file to display the basic design of the Tic Tac Toe game.
 
+## How to handle player's choice:
+This game is going to have two buttons named "Player X" and "Player O". The first player that chooses a button initiates the game, and buttons get disable at this point. 
 
-## How to handle player choice:
-This game is going to have two buttons named "Player X" and "Player O". The first player that chooses a button initiates the game, and buttons get disable at this point.
+## Set the validation of the player's choice:
+Once the symbol selection has been made on a cell it can't be changed.
+
+## Rules of the game:
+A player wins once three consecutive same symbols are fill in a row, column or diagonally. I should create a function that keeps track of each player's mark during the game to validate the winning condition.
--- script.js
+++ script.js
@@ -4,24 +4,39 @@ const row2 = ["", "", ""];
 
 const row3 = ["", "", ""];
 
+let message = "Game started!";
+
+let state = "Not started";
+
 const render = () => {
+
     for (let i = 0; i < row1.length; i++) {
         const element = row1[i];
-        const cell = document.querySelector("#r1c" + (i+1));
+        const cell = document.querySelector("#r1c" + (i + 1));
         cell.setAttribute("src", element + ".png");
     }
-    
+
     for (let i = 0; i < row2.length; i++) {
         const element = row2[i];
-        const cell = document.querySelector("#r2c" + (i+1));
+        const cell = document.querySelector("#r2c" + (i + 1));
         cell.setAttribute("src", element + ".png");
     }
-    
+
     for (let i = 0; i < row3.length; i++) {
         const element = row3[i];
-        const cell = document.querySelector("#r3c" + (i+1));
+        const cell = document.querySelector("#r3c" + (i + 1));
         cell.setAttribute("src", element + ".png");
     }
+
+    if (currentPlayer !== "") {
+        document.querySelector("#playerSelection").setAttribute("style", "display: none");
+        document.querySelector("#board").setAttribute("style", "display: block");
+    } else {
+        document.querySelector("#playerSelection").setAttribute("style", "display: block");
+        document.querySelector("#board").setAttribute("style", "display: none");
+    }
+
+    document.querySelector("#message").innerText = message;
 }
 
 const clearBoard = () => {
@@ -44,25 +59,86 @@ const chooseX = document.querySelector("#buttonX");
 
 chooseX.addEventListener("click", () => {
     currentPlayer = "X";
-    document.querySelector("#playerSelection").setAttribute("style", "display: none");
-    document.querySelector("#board").setAttribute("style", "display: block");
+    state = "started";
+    render();
 });
 
 const chooseO = document.querySelector("#buttonO");
 
 chooseO.addEventListener("click", () => {
     currentPlayer = "O";
-    document.querySelector("#playerSelection").setAttribute("style", "display: none");
-    document.querySelector("#board").setAttribute("style", "display: block");
+    state = "started";
+    render();
 });
 
 const clickCell = (row, column) => {
+    if (state === "finished") {
+        return;
+    }
+
+    if (row[column] !== "Blank") {
+        return;
+    }
+
     row[column] = currentPlayer;
+
+    validate();
+
     if (currentPlayer === "O") {
         currentPlayer = "X";
     } else {
         currentPlayer = "O";
     }
+
+    if (state !== "finished") {
+        message = `${currentPlayer} turn!`;
+    }
+
+    render();
+}
+
+const validateRows = (array) => {
+    const result = array.filter(mark => mark === currentPlayer);
+    if (result.length === 3) {
+        message = `${currentPlayer} won. Game over!`;
+        state = "finished";
+    }
+}
+
+const validateColumns = (index) => {
+    if (row1[index] === currentPlayer && row2[index] === currentPlayer && row3[index] === currentPlayer) {
+        message = `${currentPlayer} won. Game over!`;
+        state = "finished";
+    }
+}
+
+const validateDiagonal = () => {
+    if ((row1[0] === currentPlayer && row2[1] === currentPlayer && row3[2] === currentPlayer) || 
+        (row1[2] === currentPlayer && row2[1] === currentPlayer && row3[0] === currentPlayer)) {
+        message = `${currentPlayer} won. Game over!`;
+        state = "finished";
+    }
+}
+
+const validate = () => {
+    // Validating rows:
+    validateRows(row1);
+    validateRows(row2);
+    validateRows(row3);
+    // Validating columns:
+    validateColumns(0);
+    validateColumns(1);
+    validateColumns(2);
+
+    // Validating diagonal:
+    validateDiagonal();
+}
+
+const restartGame = () => {
+    clearBoard();
+    message = "Game started!";
+    state = "Not started";
+    currentPlayer = "";
     render();
 }
 
